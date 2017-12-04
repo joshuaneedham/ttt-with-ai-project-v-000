d5a98934aa99591c794588c05565482e5faaf13b
Joshua Needham <joshuabneedham@gmail.com>
2017-12-04 17:44:40 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -51,15 +51,17 @@ class Game
       turn
     end
     if won?
-      puts "COngratulations #{winner}!"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
-  end      
+  end
 
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]+1)
+      @board.cells[combo[0]] == @board.cells[combo[1]] &&
+      @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0]+1)
     end
   end
 
